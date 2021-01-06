78e04e25912c772adbe32f3ed330539d4ca9716b
Jacob Whisler <jwhisler521@gmail.com>
2021-01-05 19:39:16 -0500

    rendered game detail data onto the UI

---
+++ src/components/GameDetail.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+import { useSelector } from "react-redux";
+
+const GameDetail = () => {
+  // Data
+  const { screen, game } = useSelector((state) => state.detail);
+  return (
+    <div className="card-shadow">
+      <div className="detail">
+        <div className="stats">
+          <div className="rating">
+            <h3>{game.name}</h3>
+            <p>Rating: {game.rating}</p>
+          </div>
+          <div className="info">
+            <h3>Platforms</h3>
+            <div className="platforms">
+              {console.log(game.platforms)}
+              {game.platforms.map((data) => (
+                <h3 key={data.platform.id}>{data.platform.name}</h3>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="media">
+          <img src={game.background_image} alt="image" />
+        </div>
+        <div className="description">{game.description_raw}</div>
+        <div className="gallery">
+          {console.log(screen.results)}
+          {screen.results.map((screen) => (
+            <img key={screen.id} src={screen.image} alt="image" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameDetail;
