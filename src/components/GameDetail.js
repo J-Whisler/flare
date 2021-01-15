5d6d2b3a03db445ce357d828448a0abf39c36524
Jacob Whisler <jwhisler521@gmail.com>
2021-01-14 20:44:49 -0500

    deleted unused logo.svg import

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -12,7 +12,6 @@ import playstation from "../img/playstation.svg";
 import steam from "../img/steam.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
-import logo from "../img/logo.svg";
 import nintendo from "../img/nintendo.svg";
 import xbox from "../img/xbox.svg";
 import starEmpty from "../img/star-empty.png";
