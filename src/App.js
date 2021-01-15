dbe47f17df3c21fee27b4755d5ae17809ec33c8a
Jacob Whisler <jwhisler521@gmail.com>
2021-01-14 20:43:44 -0500

    rendering Nav component onto UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+
 import Home from "./pages/Home";
+import Nav from "./components/Nav";
+
 import GlobalStyles from "./components/GlobalStyles";
+
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <Home />
       <Route path={["/game/:id", "/"]}></Route>
     </div>
