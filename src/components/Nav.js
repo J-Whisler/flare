55bbc98da9a3901dcea1426dc14564f14be1e6ac
Jacob Whisler <jwhisler521@gmail.com>
2021-01-17 21:21:45 -0500

    added functionality to search bar and logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,56 +1,82 @@
-import React from "react";
-
+import React, { useState } from "react";
+//Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import logo from "../img/logo.svg";
+//Redux and Routes
+import { fetchSearch } from "../actions/gamesAction";
+import { useDispatch } from "react-redux";
+import { fadeIn } from "../animations";
 
 const Nav = () => {
+  const dispatch = useDispatch();
+  const [textInput, setTextInput] = useState("");
+
+  const inputHandler = (e) => {
+    setTextInput(e.target.value);
+  };
+  const submitSearch = (e) => {
+    e.preventDefault();
+    dispatch(fetchSearch(textInput));
+    setTextInput("");
+  };
+  const clearSearched = () => {
+    dispatch({ type: "CLEAR_SEARCHED" });
+  };
   return (
-    <StyledNav>
-      <Logo>
+    <StyledNav variants={fadeIn} initial="hidden" animate="show">
+      <Logo onClick={clearSearched}>
         <img src={logo} alt="logo" />
-        <h1>Flare</h1>
+        <h1>Ignite</h1>
       </Logo>
-      <div className="search">
-        <input type="text" />
-        <button>Search</button>
-      </div>
+      <form className="search">
+        <input value={textInput} onChange={inputHandler} type="text" />
+        <button onClick={submitSearch} type="submit">
+          Search
+        </button>
+      </form>
     </StyledNav>
   );
 };
 
-const StyledNav = styled(motion.div)`
+const StyledNav = styled(motion.nav)`
   padding: 3rem 5rem;
   text-align: center;
+
   input {
-    width: 60%;
+    width: 50%;
     font-size: 1.5rem;
     padding: 0.5rem;
     border: none;
-    border-radius: 1rem;
     margin-top: 1rem;
     box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
+    border-radius: 2rem;
   }
   button {
     font-size: 1.5rem;
     border: none;
     padding: 0.5rem 2rem;
     cursor: pointer;
-    color: #ca743a;
-    background: #353d3a;
+    color: #b76935;
+    background: #143642;
+    border-radius: 0rem 2rem 2rem 0rem;
     position: relative;
-    right: 8.5rem;
-    border-radius: 0rem 1rem 1rem 0rem;
+    right: 1rem;
+    box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
   }
 `;
+
 const Logo = styled(motion.div)`
   display: flex;
   justify-content: center;
-  /* color: #ca743a; */
+  padding: 1rem;
+  position: relative;
+  right: 0.5rem;
+  cursor: pointer;
   img {
-    width: 2rem;
     height: 2rem;
-    /* background-color: #ca743a; */
+    width: 2rem;
   }
 `;
+
 export default Nav;
